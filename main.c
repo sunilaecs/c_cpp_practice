05575c382b81048506c9939dd62edfe0eb7ed8f2
Sunil Kanupuru <sunil.kanupuru@gmail.com>
2019-04-21 19:25:46 +0530

    initial project version

---
+++ main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    //int x;
+    //int y;
+    float z = 8900.768;
+    //printf("Please enter two numbers to add:  \n");
+    //scanf("%d",&x);
+    //scanf("%d",&y);
+    //printf("Hello worlds!: %d \n",x+y);
+    printf("%f \n", z);
+    printf("%10f \n", z);
+
+    printf("%6f",z);
+
+    return 0;
+}
